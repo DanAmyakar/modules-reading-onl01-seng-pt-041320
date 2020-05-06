0add5a50c4571cae5d43a8b6daa6946e92ad3743
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-06 17:53:55 +0000

    Automatically backed up by Learn

---
+++ lib/dance_module.rb
@@ -0,0 +1,19 @@
+module Dance
+  
+  def twirl
+    "I'm twirling!"
+  end
+  
+  def jump
+    "Look how high I'm jumiping!"
+  end
+  
+  def piroutte
+    "I'm doing a piroutte"
+  end
+  
+  def take_a_bow
+    "Thank you, thank you. It was a please to dance for you all."
+  end
+  
+end
